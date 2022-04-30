31a91188f6302415ffa34113817661131baf0b63
Gabriel Silveira <gabriel@gabrielsilveira.com.br>
2022-04-30 20:49:10 -0300

    Central profile picture

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,11 @@
 import { Component } from 'react';
 
-import '../assets/scss/home.scss';
+import '../assets/scss/section-transitions.scss';
+import '../assets/sass/home.sass';
 
 import Transitions from '../services/page-transition';
+
+import ProfilePicture from '../assets/images/gabriel-silveira.jpg';
 
 class HomePage extends Component {
   componentDidMount() {
@@ -11,27 +14,45 @@ class HomePage extends Component {
 
   render() {
     return (
-      <main className="main">
-        <section className="section">
-          <div className="close-section">&times;</div>
-          <div className="demo-box">About</div>
-        </section>
+      <div>
+        <img
+          src={ProfilePicture}
+          alt="Gabriel Silveira"
+          style={{
+            zIndex: 999,
+            position: "absolute",
+            left: '50%',
+            top: '50%',
+            marginLeft: -100,
+            marginTop: -100,
+            width: 200,
+            borderRadius: 100,
+            border: '10px solid rgba(0, 0, 0, 0.1)',
+          }}
+        />
 
-        <section className="section">
-          <div className="close-section">&times;</div>
-          <div className="demo-box">Experience</div>
-        </section>
+        <main className="main">
+          <section className="section">
+            <div className="close-section">&times;</div>
+            <div className="demo-box">About</div>
+          </section>
 
-        <section className="section">
-          <div className="close-section">&times;</div>
-          <div className="demo-box">Skills</div>
-        </section>
+          <section className="section">
+            <div className="close-section">&times;</div>
+            <div className="demo-box">Experience</div>
+          </section>
 
-        <section className="section">
-          <div className="close-section">&times;</div>
-          <div className="demo-box">Contact Me</div>
-        </section>
-      </main>
+          <section className="section">
+            <div className="close-section">&times;</div>
+            <div className="demo-box">Skills</div>
+          </section>
+
+          <section className="section">
+            <div className="close-section">&times;</div>
+            <div className="demo-box">Contact Me</div>
+          </section>
+        </main>
+      </div>
     )
   }
 }
