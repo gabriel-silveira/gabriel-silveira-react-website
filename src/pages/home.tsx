93f6edf9d5740eececaa1920ff7cee876d5b73e3
Gabriel Silveira <gabriel@gabrielsilveira.com.br>
2022-05-06 08:59:44 -0300

    Cleaning code

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 import '../assets/scss/section-transitions.scss';
 
