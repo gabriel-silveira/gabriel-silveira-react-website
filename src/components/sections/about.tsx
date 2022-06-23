3fb29b36ae01c073cc25671ea1e0a4e62f36620c
Gabriel Silveira <gabriel@gabrielsilveira.com.br>
2022-06-23 12:52:11 +0100

    i18n text on about section

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -3,34 +3,23 @@ import { useTranslation } from "react-i18next";
 function AboutSection() {
   const { t } = useTranslation();
 
-  const linkedinUrl = 'https://www.linkedin.com/in/gabriel-silveira/'
-
   return (
     <div style={ { width: '50%' } }>
       <h1>{t(`home.sections.about`)}</h1>
 
-      <p>
-        Hi! My name is &lt;Gabriel Silveira&gt; and i'm a web developer, based in Nazaré, Portugal.
-      </p>
+      <p>{t(`about.p1`)}</p>
 
-      <p>
-        I've been working in software development for over 20 years with
-        a greater focus on front-end.
-      </p>
+      <p>{t(`about.p2`)}</p>
 
-      <p>
-        In recent years specialized in building graphical tools
-        such as vector graphics editors, flow diagrams, seat
-        maps and chatbot builders using Canvas API, SVG, Fabric.js,
-        Joint.js among others.
-      </p>
+      <p>{t(`about.p3`)}</p>
 
-      <p>I'm proud to have collaborated with some awesome companies:</p>
+      <p>{t(`about.p4`)}</p>
 
-      <p>
-        I’m always open to discussing new projects or partnership opportunities,
-        so feel free to get in touch by e-mail or phone.
-      </p>
+      <div>
+        UMode, Vertical Digital, Ubistart, Inti, Cartórios de Protesto
+      </div>
+
+      <p>{t(`about.p5`)}</p>
     </div>
   )
 }
--- src/services/i18n/languages/en/index.js
+++ src/services/i18n/languages/en/index.js
@@ -8,5 +8,12 @@ export default {
         contact: 'Contact Me',
       },
     },
+    about: {
+      p1: 'Hi! My name is &lt;Gabriel Silveira&gt; and i\'m a web developer based in Nazaré, Portugal.',
+      p2: 'I\'ve been working in software development for over 20 years with a greater focus on front-end.',
+      p3: 'In recent years specialized in building graphical tools such as vector graphics editors, flow diagrams, seat maps and chatbot builders using Canvas API, SVG, Fabric.js, Joint.js among others.',
+      p4: 'I\'m proud to have collaborated with some awesome companies:',
+      p5: 'I’m always open to discussing new projects or partnership opportunities, so feel free to get in touch by e-mail or phone.',
+    },
   },
 };
